cbf29e75a162b5627e11686a3c5d187cf3a6b481
cj007 <cj007@cj2023M2>
2023-12-18 11:21:01 -0600

    Initial Commit

---
+++ HelloWorld/src/Hello.java
@@ -0,0 +1,20 @@
+
+public class Hello {
+
+	public static void main(String[] args) {
+		String firstName = "Jose";
+		String lastName = "Hernandez";
+		String streetName = "1234 Chicago Street";
+		String city = "Chicago";
+		String zipCode = "60605";
+		
+		System.out.println("First Name: " + firstName);
+		System.out.println("Last Name: " + lastName);
+		System.out.println("Street Address: " + streetName);
+		System.out.println("City: " + city);
+		System.out.println("Zip Code: " + zipCode);
+		
+
+	}
+
+}
